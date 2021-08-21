f95020283243b86e08bdeb484c1c4e154f18f2e6
khaled <shnbyakuya@gmail.com>
2021-08-21 23:01:04 +0100

    final UI version ~ no validation

---
+++ app.js
@@ -0,0 +1,15 @@
+// const form = document.querySelector("#form");
+// const email = document.querySelector("#email");
+// const button = document.querySelector("#submit");
+// let mail = email.value;
+// button.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   form.classList.remove("invalid");
+
+//   let regx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//   let result = mail.match(regx);
+
+//   if (!result) {
+//     form.classList.add("invalid");
+//   }
+// });
